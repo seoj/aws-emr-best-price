f946a229a2f5d33f4251186d43d0c4276d637627
seoj <jmseo2@gmail.com>
2015-06-20 14:17:21 -0400

    - Moved on-demand price retrieval logic into separate object. - Create object to encapsulate an on-demand price. - Added headers to on-demand price CSV.

--- src/main/java/my/seoj/aws/emr/best/price/Main.java
+++ src/main/java/my/seoj/aws/emr/best/price/Main.java
@@ -1,15 +1,8 @@
 package my.seoj.aws.emr.best.price;
 
 import java.io.FileWriter;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.math.BigDecimal;
 import java.util.Date;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-
-import org.apache.commons.io.IOUtils;
 
 import au.com.bytecode.opencsv.CSVWriter;
 
@@ -18,50 +11,22 @@ import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeSpotPriceHistoryRequest;
 import com.amazonaws.services.ec2.model.DescribeSpotPriceHistoryResult;
 import com.amazonaws.services.ec2.model.SpotPrice;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 
 public class Main
 {
    public static void main(String[] args) throws Exception
    {
+      OnDemandPrices onDemandPrices = new OnDemandPrices();
+
       try (CSVWriter csvWriter = new CSVWriter(new FileWriter("linux-od.csv")))
       {
-         String onDemandPricesJsonpString;
-         HttpURLConnection connection =
-            (HttpURLConnection) new URL("http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js?_=" + System.currentTimeMillis()).openConnection();
-         connection.setRequestMethod("GET");
-         try (InputStream responseInputStream = connection.getInputStream())
+         csvWriter.writeNext(new String[] { "region", "instanceType", "price" });
+         for (OnDemandPrice onDemandPrice : onDemandPrices.getOnDemandPrices())
          {
-            onDemandPricesJsonpString = IOUtils.toString(responseInputStream);
-         }
-         connection.disconnect();
-
-         ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByExtension("js");
-         scriptEngine.eval("function callback(obj){return JSON.stringify(obj);}");
-         String onDemandPricesJsonString = (String) scriptEngine.eval(onDemandPricesJsonpString);
-         Gson gson = new Gson();
-         JsonElement onDemandPricesJson = gson.fromJson(onDemandPricesJsonString, JsonElement.class);
-         JsonArray regionsJson = onDemandPricesJson.getAsJsonObject().get("config").getAsJsonObject().get("regions").getAsJsonArray();
-         for (JsonElement regionJson : regionsJson)
-         {
-            String region = regionJson.getAsJsonObject().get("region").getAsString();
-            JsonArray instanceTypesJson = regionJson.getAsJsonObject().get("instanceTypes").getAsJsonArray();
-            for (JsonElement instanceTypeJson : instanceTypesJson)
-            {
-               JsonArray sizesJson = instanceTypeJson.getAsJsonObject().get("sizes").getAsJsonArray();
-               for (JsonElement sizeJson : sizesJson)
-               {
-                  String size = sizeJson.getAsJsonObject().get("size").getAsString();
-                  JsonArray valueColumnsJson = sizeJson.getAsJsonObject().get("valueColumns").getAsJsonArray();
-                  for (JsonElement valueColumnJson : valueColumnsJson)
-                  {
-                     String price = valueColumnJson.getAsJsonObject().get("prices").getAsJsonObject().get("USD").getAsString();
-                     csvWriter.writeNext(new String[] { region, size, price });
-                  }
-               }
-            }
+            String region = onDemandPrice.getRegion();
+            String instanceType = onDemandPrice.getInstanceType();
+            BigDecimal price = onDemandPrice.getPrice();
+            csvWriter.writeNext(new String[] { region, instanceType, String.valueOf(price) });
          }
       }
 
